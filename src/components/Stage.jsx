6549e2a9b606cc5e2ab5600712417c16c9f297df
Amirali <57327052+itsamirali@users.noreply.github.com>
2022-08-18 23:35:31 +0430

    added gamehelper func

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Cell from './Cell'
+import Cell from "./Cell";
 
-const Stage = ({stage}) => {
+const Stage = ({ stage }) => {
   return (
     <div>
-        <Cell />
+      {stage.map((row) =>
+        row.map((cell, x) => <Cell key={x} type={cell[0]} />)
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Stage
+export default Stage;
